bf9fbb01403af8d01f31bfca00a7f55eb43f4140
Alifiyan Rosyidi <hello@ar14.works>
2024-09-20 01:28:36 +0700

    Update osxbundle.sh

--- build-aux/osx/osxbundle.sh
+++ build-aux/osx/osxbundle.sh
@@ -3,7 +3,7 @@ LASTPWD=$PWD
 
 # Handle copying all the dylibs to their respective directories
 # As well handle creating our directory structure
-cd out/Inochi\ Session.app/Contents
+cd out/Puppetstring\ Model\ Viewer.app/Contents
 
 # Remove old files
 if [ -d "Frameworks" ]; then
@@ -14,7 +14,7 @@ fi
 
 # Create new directories and move dylibs
 mkdir -p Frameworks SharedSupport Resources Resources/i18n
-mv MacOS/libSDL2*.dylib Frameworks/libSDL2.dylib
+mv MacOS/libSDL2-2.0.dylib Frameworks/libSDL2.dylib
 mv -n MacOS/*.dylib Frameworks
 
 # Move back to where we were
@@ -35,14 +35,14 @@ cp LICENSE out/Puppetstring\ Model\ Viewer.app/Contents/SharedSupport/LICENSE
 
 # Create icons dir
 # TODO: check if dir exists, skip this step if it does
-if [ ! -d "out/InochiCreator.icns" ]; then
+if [ ! -d "out/PuppetstringModelViewer.icns" ]; then
     iconutil -c icns -o out/PuppetstringModelViewer.icns build-aux/osx/PuppetstringModelViewer.iconset
 else
     echo "Icons already exist, skipping..."
 fi
 
 echo "Applying Icon..."
-cp out/InochiSession.icns out/Puppetstring\ Model\ Viewer.app/Contents/Resources/PuppetstringModelViewer.icns 
+cp out/InochiSession.icns out/Inochi\ Session.app/Contents/Resources/PuppetstringModelViewer.icns 
 
 echo "Cleaning up..."
 find out/Puppetstring\ Model\ Viewer.app/Contents/MacOS -type f ! -name "puppetstring-modelviewer" -delete
